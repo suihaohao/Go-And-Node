74af823ede06f0f03341d1081d320ca5fa259c50
suihaohao <1255151021@qq.com>
2019-07-25 15:40:50 +0800

    Update main.go

--- go/main.go
+++ go/main.go
@@ -1,14 +1,33 @@
-
+package main
 
 import (
 	"crypto/sha1"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
-  	"log"
+  "log"
 )
 
 
 func main(){
+	
   text := "123456text"
   sha1Sum := fmt.Sprintf("%x", sha1.Sum([]byte(text)))
   log.Println(sha1Sum)
+	
+	
+	
+	//加盐哈希
+	salt := "e9a51074898fb36d"
+	h := hmac.New(sha256.New, []byte(salt))
+	h.Write([]byte(text))
+	hmacResult := hex.EncodeToString(h.Sum(nil))
+	log.Println(hmacResult)
+	
+
 }
+
+
+
+

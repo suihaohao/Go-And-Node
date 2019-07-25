39d3a1619cdf7482ce054f5af6b07f1f783205f5
suihaohao <1255151021@qq.com>
2019-07-25 15:37:25 +0800

    Update main.js

--- node/main.js
+++ node/main.js
@@ -3,3 +3,14 @@ const sha1 = require("sha1");
 
 const sha1Sum = sha1(text);
 console.log(sha1Sum)
+
+
+
+//加盐哈希
+const crypto = require('crypto');
+const salt = "e9a51074898fb36d";
+const hash = crypto.createHmac('sha256', salt);
+hash.update(text);
+const hmacResult = hash.digest('hex');
+
+console.log(hmacResult)
